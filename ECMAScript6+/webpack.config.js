0baebeb6be6873212aa75292f1df86794efcc8c9
Thiago Silva Lopes <thiagodrive08@hotmail.com>
2021-04-29 13:50:58 -0300

    Funciona mas dá erro 404 no navegador :/

--- ECMAScript6+/webpack.config.js
+++ ECMAScript6+/webpack.config.js
@@ -4,10 +4,12 @@ module.exports = {
   entry: ["@babel/polyfill", path.resolve(__dirname, "./src/main.js")],
   output: {
     path: path.resolve(__dirname, "./public"),
-    filename: "bundle.js",
+    filename: "bundle.js.map",
+    sourceMapFilename: "bundle.js",
   },
   devServer: {
     contentBase: path.resolve(__dirname, "./public"),
     port: 3000,
   },
+  devtool: "source-map",
 };
